00599f796d39bbb27d092bbdb3e48038b6d6d5d8
Iskriger <pv019073@gmail.com>
2026-08-26 10:46:57 +0900

    UI interface path 1

--- lib/main.dart
+++ lib/main.dart
@@ -9,60 +9,80 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      home: const NotesListScreen(),
       title: 'Flutter Demo',
       theme: ThemeData(
-
-        colorScheme: .fromSeed(seedColor: Colors.green),
-      ),
-      home: const NotesListScreen(title: ''),
-    );
+        primaryColor: Color(0xFF0F172A),
+        scaffoldBackgroundColor: const Color(0xFF0F172A),
+        appBarTheme: const AppBarTheme(backgroundColor: Color(0xFF0F172A)),
+        floatingActionButtonTheme: const FloatingActionButtonThemeData(backgroundColor: Colors.blue),
+        textTheme: const TextTheme(bodyMedium: TextStyle(color: Colors.white)),
+        )
+        
+      );
+      
   }
 }
 
 class NotesListScreen extends StatefulWidget {
-  const NotesListScreen({super.key, required this.title});
-
-
-  final String title;
-
+  const NotesListScreen({super.key});
+  
   @override
   State<NotesListScreen> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<NotesListScreen> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(onPressed: (){
+         Navigator.push(context,
+         MaterialPageRoute(builder: (context) => const AddNoteScreen()),);
+      },
+      child: const Icon(Icons.add),
+      ),
+      body: Center(
+        child: Text("Нет записок", 
+        style: TextStyle(color:Colors.white),
+        ),
+        ),
+    );
   }
+}
 
+class AddNoteScreen extends StatelessWidget {
+  const AddNoteScreen({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
+        title: const Text("Новая записка"),
+        leading: IconButton(icon: Icon(Icons.close),
+        onPressed: () => Navigator.pop(context),
+        ),
+        ),
+        body: Padding(padding: EdgeInsets.all(16),
         child: Column(
-         
-          mainAxisAlignment: .center,
           children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            TextField(
+              
+              decoration: InputDecoration(labelText: 'Заголовок', border: OutlineInputBorder()),
+              ),
+              SizedBox(height: 16),
+              Expanded(child: TextField(
+                maxLines: null,
+                expands: true,
+                decoration: InputDecoration(labelText: "Текст", border: OutlineInputBorder()),
+                ),
+              ),
+              SizedBox(height: 16),
+              ElevatedButton(onPressed: () => Navigator.pop(context), child: Text("Сохранить"),
+              ),
+            
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+        ),
+        
+      );
   }
 }
